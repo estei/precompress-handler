bf929fae174b4bece07b2e966e1dc958736a68ce
Rasmus Lauridsen <rasmus@lauridsen.me>
2016-01-04 00:55:53 +0100

    gzip or not
    still lots o work

---
+++ src/PrecompressedAssets/PrecompressedHandler.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Web;
+
+namespace PrecompressedAssets
+{
+    public class PrecompressedHandler : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            var uncompressedfile = context.Request.PhysicalPath;
+            var compressedFile = uncompressedfile + ".gz";
+            //Does the client support gzip?
+            if (context.Request.AcceptsGzip())
+            {
+                if (File.Exists(compressedFile))
+                {
+                    context.Response.WriteFile(compressedFile);
+                    context.Response.Headers.Add("Content-Encoding", "gzip");
+                    //contenttype
+                    //conditional headers?
+                    //Cache client?
+                    //cache server?
+                    return;
+                }
+            }
+            if (File.Exists(uncompressedfile))
+            {
+                context.Response.WriteFile(uncompressedfile);
+                //contenttype
+                //conditional headers?
+                //Cache client?
+                //cache server?
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        }
+
+        public bool IsReusable => false;
+    }
+}
---
+++ src/PrecompressedAssets/RequestExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+
+namespace PrecompressedAssets
+{
+    public static class RequestExtensions
+    {
+        public static bool AcceptsGzip(this HttpRequest request)
+        {
+            var acceptEncoding = request.Headers["Accept-Encoding"];
+            return !string.IsNullOrWhiteSpace(acceptEncoding) && acceptEncoding.Contains("gzip");
+        }
+    }
+}
